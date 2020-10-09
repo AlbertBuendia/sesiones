dddc8bbaf42fc542092550bc8ef8901045dba4e9
Albert Buendia <6235.joan23@fje.edu>
2020-10-09 17:11:52 +0200

    Primera subida

--- services/login.proc.php
+++ services/login.proc.php
@@ -6,6 +6,9 @@ if (isset($_POST['email']) /*&& isset($_POST['psswd']*/) {
     $psswd=md5($_POST['psswd']);
     $query='SELECT * from tb_user where email="'.$email.'" AND passwd="'.$psswd.'"';
     $result=mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_array($result)){
+        
+    }
 } else {
     header('Location: ../index.php');
 }
