e5110814bef1ef91962b9b871c909dbb75a0b7d0
Albert Buendia <6235.joan23@fje.edu>
2020-10-09 18:33:38 +0200

    Primera subida

--- services/session.php
+++ services/session.php
@@ -3,4 +3,6 @@ session_start();
 if (!isset($_SESSION['email_user'])) {
     header('Location: ../index.php');    
 }
+echo '<h1>Bienvenido '.$_SESSION['email_user'].'</h1>';
+echo '<a href="../services/logout.proc.php">Cerrar sesion</a>';
 ?>
--- view/home.php
+++ view/home.php
@@ -6,8 +6,6 @@
 
 <?php
 include '../services/session.php';
-echo '<h1>Bienvenido '.$_SESSION['email_user'].'</h1>';
-echo '<a href="../services/logout.proc.php">Cerrar sesion</a>';
 ?>
 
 <div>
